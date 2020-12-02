303c460bd62e3c3dcf2862d472ba2deb86bf2b80
Jon Richards <jrseti@gmail.com>
2020-12-02 09:42:47 -0800

    Added drop

--- modules/module10/google_app.py
+++ modules/module10/google_app.py
@@ -11,6 +11,8 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
    doodle_info_to_render = mongo.db.doodle_info.find_one()
+   if doodle_info_to_render is None:
+    return "Hey! You need to first visit <a href='http://127.0.0.1:5000/scrape'>http://127.0.0.1:5000/scrape</a>"
    return render_template("doodle.html", doodle=doodle_info_to_render)
 
 @app.route("/scrape")
@@ -18,8 +20,13 @@ def scrape():
    doodle_info_collection = mongo.db.doodle_info
    doodle_data = google_scrape.scrape()
    doodle_info_collection.update({}, doodle_data, upsert=True)
-   return "Scraping Successful!"
+   return "Scraping Successful! To view the results go to <a href='http://127.0.0.1:5000'>http://127.0.0.1:5000</a>"
   
+@app.route("/drop")
+def drop():
+    mongo.db.doodle_info.drop()
+    return "Dropped! You will need to scrape again: <a href='http://127.0.0.1:5000/scrape'>http://127.0.0.1:5000/scrape</a>"
+
 @app.route("/hello")
 def hello():
     return "HELLO"
